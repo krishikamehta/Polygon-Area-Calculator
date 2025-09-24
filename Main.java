268a74b600c482024122dc2b091b7d80cff0e4d0
krishikamehta <krishikamehta007@gmail.com>
2025-09-24 14:54:31 +0530

    Updated Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import gui.HomePanel;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Polygon Area Calculator - Hardik Batra");
+            JFrame frame = new JFrame("Polygon Area Calculator - Krishika Mehta");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(1000, 700);
             frame.setLocationRelativeTo(null);
